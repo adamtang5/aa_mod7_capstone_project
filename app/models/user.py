932f2a92a9d7358a4157d35dbc12209b1ab871ef
Adam Tang <adamtang@gmail.com>
2022-05-10 15:43:44 -0700

    updated columns in user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,20 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    # username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    display_name = db.Column(db.String(50), nullable=False)
+    avatar_url = db.Column(db.String)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
 
     @property
     def password(self):
@@ -25,6 +30,10 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            # 'username': self.username,
+            'email': self.email,
+            'display_name': self.display_name,
+            'avatar_url': self.avatar_url,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
