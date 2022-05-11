ffd8618ba22d5f33dd3bc56323aabf032f1e1d1f
Adam Tang <adamtang@gmail.com>
2022-05-11 07:33:25 -0700

    migrated and upgraded Status model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .db import db
 from .user import User
 from .project import Project
 from .issue_type import IssueType
+from .status import Status
---
+++ app/models/status.py
@@ -0,0 +1,18 @@
+from .db import db
+import json
+
+
+class Status(db.Model):
+    __tablename__ = 'Statuses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String, nullable=False)
+    adjacency_list = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'type': self.type,
+            'adjacency_list': json.loads(adjacency_list)
+        }
