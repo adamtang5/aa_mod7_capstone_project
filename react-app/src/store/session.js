c3ed46be940de71115390eddb78b3b22236186e0
Adam Tang <adamtang@gmail.com>
2022-05-12 13:23:19 -0700

    fixed flask form variable name mismatch

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -96,16 +96,17 @@ export const logout = () => async (dispatch) => {
 };
 
 
-export const signUp = (username, email, password) => async (dispatch) => {
+export const signUp = (email, password, displayName, avatarUrl) => async (dispatch) => {
   const response = await fetch('/api/auth/signup', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      username,
       email,
       password,
+      display_name: displayName,
+      avatar_url: avatarUrl,
     }),
   });
 
