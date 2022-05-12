e0f30d4357ec1c7b349a8ee5c472e7aec771023c
Adam Tang <adamtang@gmail.com>
2022-05-12 14:08:36 -0700

    implemented Roles and move issue-user rels into JoinIU

--- app/models/join_i_u.py
+++ app/models/join_i_u.py
@@ -4,5 +4,6 @@ from .db import db
 issues_users = db.Table(
     "JoinIU",
     db.Column("issue_id", db.Integer, db.ForeignKey("Issues.id"), primary_key=True),
-    db.Column("user_id", db.Integer, db.ForeignKey("Users.id"), primary_key=True)
+    db.Column("user_id", db.Integer, db.ForeignKey("Users.id"), primary_key=True),
+    db.Column("role_id", db.Integer, db.ForeignKey("Roles.id"))
 )
--- app/models/user.py
+++ app/models/user.py
@@ -30,9 +30,6 @@ class User(db.Model, UserMixin):
         back_populates="users"
     )
 
-    submitted_issues = db.relationship("Issue", foreign_keys="[Issue.submitter_id]", back_populates="submitter")
-    assigned_issues = db.relationship("Issue", foreign_keys="[Issue.assignee_id]", back_populates="assignee")
-
     @property
     def password(self):
         return self.hashed_password
