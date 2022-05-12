aea9882460c6c188c6613db66ed4814b979fcb75
Adam Tang <adamtang@gmail.com>
2022-05-12 15:39:14 -0700

    user page components

---
+++ react-app/src/components/Icons/Avatar/index.js
@@ -0,0 +1,17 @@
+import "./Avatar.css";
+
+const Avatar = ({ user }) => {
+    return (
+        <a href={`/users/${user?.id}`}>
+            <div className="avatar">
+                {user?.avatar_url ? (
+                    <img src={user?.avatar_url} alt={user?.display_name} />
+                ) : (
+                    <div className="avatar-placeholder" />
+                )}
+            </div>
+        </a>
+    );
+};
+
+export default Avatar;
