b42974f8583c60ac9dad7057feabd110dbdb3ba6
Adam Tang <adamtang@gmail.com>
2022-05-11 22:12:59 -0700

    copied Modal code

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,72 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal } from './Modal';
 import { NavLink } from 'react-router-dom';
+import LoginForm from './auth/LoginForm';
+import SignUpForm from './auth/SignUpForm';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
+
+  const switchToSignupForm = e => {
+    e.preventDefault();
+    setShowLoginModal(false);
+    setShowSignupModal(true);
+  };
+
+  const switchToLoginForm = e => {
+    e.preventDefault();
+    setShowSignupModal(false);
+    setShowLoginModal(true);
+  };
+
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+      <div>
+        <NavLink to='/' exact={true} activeClassName='active'>
+          Home
+        </NavLink>
+      </div>
+      <div className="auth-modal">
+        <span
+          className="nav-button"
+          onClick={() => setShowLoginModal(true)}
+        >Log In</span>
+        {showLoginModal && (
+          <Modal onClose={() => setShowLoginModal(false)}>
+            <LoginForm
+              handleSwitchForm={switchToSignupForm}
+              formTitle="Welcome back."
+            />
+          </Modal>
+        )}
+      </div>
+
+      <div className="auth-modal">
+        <span
+          className="nav-button"
+          onClick={() => setShowSignupModal(true)}
+        >Sign Up</span>
+        {showSignupModal && (
+          <Modal onClose={() => setShowSignupModal(false)}>
+            <SignUpForm
+              handleSwitchForm={switchToLoginForm}
+              formTitle="Join Mira."
+            />
+          </Modal>
+        )}
+      </div>
+
+      <div>
+        <NavLink to='/users' exact={true} activeClassName='active'>
+          Users
+        </NavLink>
+      </div>
+      <div>
+        <LogoutButton />
+      </div>
     </nav>
   );
 }
