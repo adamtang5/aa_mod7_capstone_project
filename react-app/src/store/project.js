76d6c771dcaaa94ec20d278419ff097fb9364375
Adam Tang <adamtang@gmail.com>
2022-05-14 16:24:34 -0700

    added createProject thunk

--- react-app/src/store/project.js
+++ react-app/src/store/project.js
@@ -27,6 +27,29 @@ export const fetchProjects = () => async (dispatch) => {
     }
 };
 
+export const createProject = (project) => async (dispatch) => {
+    const res = await fetch(`/api/projects/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(project),
+    });
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(newProject(data));
+        return data;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return data.errors;
+        }
+    } else {
+        return ['An error occurred. Please try again.'];
+    }
+};
+
 export const editProject = (project) => async (dispatch) => {
     const res = await fetch(`/api/projects/${project.id}`, {
         method: 'PUT',
