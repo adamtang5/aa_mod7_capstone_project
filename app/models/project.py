15f0dc997a4f70a0113a134614054a88352d2846
Adam Tang <adamtang@gmail.com>
2022-05-17 09:54:26 -0700

    project instances can generate issue idx

--- app/models/project.py
+++ app/models/project.py
@@ -2,6 +2,19 @@ from .db import db
 from .join_u_p import users_projects
 
 class Project(db.Model):
+    __slots__ = ['_number_of_issues']
+
+    def __init__(self):
+        self._number_of_issues = 0
+
+    @property
+    def number_of_issues(self):
+        return self._number_of_issues
+
+    def generate_next_idx(self):
+        self._number_of_issues += 1
+        return self._number_of_issues
+
     __tablename__ = 'Projects'
 
     id = db.Column(db.Integer, primary_key=True)
