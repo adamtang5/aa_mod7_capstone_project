fc61f23a5578ef39929da705556d4176f4e8205d
Adam Tang <adamtang@gmail.com>
2022-05-19 10:28:22 -0700

    implemented SELF as key

--- react-app/src/components/Issue/IssueRow.js
+++ react-app/src/components/Issue/IssueRow.js
@@ -1,14 +1,20 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import UserCard from "../UserCard";
 
 const IssueRow = ({ issue, idx }) => {
+    const sessionUser = useSelector(state => state.session.user);
+
     return (
         <tr
             key={issue?.id}
             className={`data-row${idx % 2 === 0 ? 'even' : 'odd'}`}
         >
             <td className="data-cell">
-                <NavLink to={`/issues/${issue?.id}`}>{issue?.project?.key}-{issue?.project_idx}</NavLink>
+                <NavLink to={`/issues/${issue?.id}`}>
+                    {(issue?.project_personal && Number(issue?.project_key.slice(1)) === sessionUser?.id)
+                        ? 'SELF' : issue?.project_key}-{issue?.project_idx}
+                </NavLink>
             </td>
             <td className="data-cell">
                 <NavLink to={`/issues/${issue?.id}`}>{issue?.title}</NavLink>
