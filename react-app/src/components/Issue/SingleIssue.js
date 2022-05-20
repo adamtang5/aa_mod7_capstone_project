726a8a80a1cf45d3c8c97d919c3d2cbb62703a25
Adam Tang <adamtang@gmail.com>
2022-05-19 22:52:16 -0700

    issue title display/edit toggle complete
    styling SingleIssue page

--- react-app/src/components/Issue/SingleIssue.js
+++ react-app/src/components/Issue/SingleIssue.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import BackCard from "../BackCard";
@@ -10,7 +10,34 @@ const SingleIssue = () => {
     const issue = useSelector(state => state.issues[+issueId]);
     const [showDisplayTitle, setShowDisplayTitle] = useState(true);
     const [showEditTitle, setShowEditTitle] = useState(false);
+    const [showTitleActions, setShowTitleActions] = useState(false);
     const [title, setTitle] = useState(issue?.title);
+
+    useEffect(() => {
+        if (!showEditTitle) return;
+
+        const closeEditTitle = () => {
+            setShowEditTitle(false);
+            setShowTitleActions(false);
+            setShowDisplayTitle(true);
+        };
+
+        document.addEventListener('click', closeEditTitle);
+
+        return () => document.removeEventListener('click', closeEditTitle);
+    }, [showEditTitle]);
+
+    const toggleTitleToEdit = e => {
+        setShowDisplayTitle(false);
+        setShowEditTitle(true);
+        setShowTitleActions(true);
+    };
+
+    const toggleTitleToDisplay = e => {
+        setShowDisplayTitle(true);
+        setShowEditTitle(false);
+        setShowTitleActions(false);
+    };
 
     return (
         <div className="issue-page-container flex-row">
@@ -21,7 +48,10 @@ const SingleIssue = () => {
                 </nav>
                 <div className="issue-title">
                     {showDisplayTitle && (
-                        <div className="display-title">
+                        <div
+                            className="display-title cursor-text"
+                            onClick={toggleTitleToEdit}
+                        >
                             {issue?.title}
                         </div>
                     )}
@@ -31,6 +61,23 @@ const SingleIssue = () => {
                             onChange={e => setTitle(e.target.value)}
                             value={title}
                         />
+                    )}
+                    {showTitleActions && (
+                        <div
+                            className="title-actions flex-row"
+                        >
+                            <div
+                                className="title-action-buttons cursor-pointer"
+                            >
+                                <i className="fa-solid fa-check" />
+                            </div>
+                            <div
+                                className="title-action-buttons cursor-pointer"
+                                onClick={toggleTitleToDisplay}
+                            >
+                                <i className="fa-solid fa-xmark" />
+                            </div>
+                        </div>
                     )}
                 </div>
             </div>
