16d7e91dbba2b6e7b0bce52381f5a98ae24cffd8
Adam Tang <adamtang@gmail.com>
2022-05-23 00:09:05 -0700

    splash footer animation done

--- react-app/src/components/SplashPage/SplashFooter.js
+++ react-app/src/components/SplashPage/SplashFooter.js
@@ -1,22 +1,43 @@
+import React, { useState } from 'react';
 import TechIconsPanel from "./TechIconsPanel";
 
 const SplashFooter = () => {
+    const [panelOpen, setPanelOpen] = useState(false);
+
+    const togglePanel = e => {
+        setPanelOpen(!panelOpen);
+    }
+
     return (
         <div className="splash-footer bounded-1200 centered flex-row">
-            <div className="tech-icon">
-                <a href="https://github.com/adamtang5/aa_mod7_capstone_project" target="_blank" rel="noreferrer">
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/github/github-original.svg" alt="project on github" />
-                </a>
+            <div className="top-layer flex-row">
+                <div className="tech-icon">
+                    <a href="https://github.com/adamtang5/aa_mod7_capstone_project" target="_blank" rel="noreferrer">
+                        <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/github/github-original.svg" alt="project on github" />
+                    </a>
+                </div>
+                <div className="creator-card">
+                    <a href="https://github.com/adamtang5/" target="_blank" rel="noreferrer">
+                        <div className="tech-icon">
+                            <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/github/github-original.svg" alt="project on github" />
+                        </div>
+                        <div className="creator-avatar">
+                            <img src="https://avatars.githubusercontent.com/u/95706331?v=4" alt="Adam Tang" />
+                        </div>
+                    </a>
+                </div>
+                <div className="creator-card">
+                    <a href="https://linkedin.com/in/adamtangx/" target="_blank" rel="noreferrer">
+                        <div className="tech-icon">
+                            <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/linkedin/linkedin-original.svg" alt="project on github" />
+                        </div>
+                        <div className="creator-avatar">
+                            <img src="https://avatars.githubusercontent.com/u/95706331?v=4" alt="Adam Tang" />
+                        </div>
+                    </a>
+                </div>
+                <TechIconsPanel />
             </div>
-            <div className="creator-card">
-                <a href="https://github.com/adamtang5/" target="_blank" rel="noreferrer">
-                    <div className="creator-avatar">
-                        <img src="https://avatars.githubusercontent.com/u/95706331?v=4" alt="Adam Tang" />
-                    </div>
-                </a>
-            </div>
-            <h3 className="footer-text">Created With: </h3>
-            <TechIconsPanel />
         </div>
     )
 };
--- react-app/src/components/SplashPage/TechIconsPanel.js
+++ react-app/src/components/SplashPage/TechIconsPanel.js
@@ -1,26 +1,16 @@
-import React, { useState } from 'react';
 import TechCard from '../TechCard';
 import techs from '../TechCard/techs.json';
 
 const TechIconsPanel = () => {
-    const [panelOpen, setPanelOpen] = useState(false);
-
-    const togglePanel = e => {
-        setPanelOpen(!panelOpen);
-    }
-
     return (
-        <>
-            <div
-                className="panel-toggle cursor-pointer"
-                onClick={togglePanel}
-            >{panelOpen ? '<' : '>'}</div>
+        <div className="fade-in flex-row">
+            <h3 className="footer-text">Created With: </h3>
             <div className="techs flex-row">
                 {techs?.map(tech => (
                     <TechCard key={tech.name} tech={tech} />
                 ))}
             </div>
-        </>
+        </div>
     )
 };
 
