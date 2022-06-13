0d39ff9d1f9f83779fda1758a3ab68227b6a326c
Adam Tang <adamtang@gmail.com>
2022-06-13 09:54:13 -0700

    made footer similar to Jira splash footer

--- react-app/src/components/SplashPage/SplashFooter.js
+++ react-app/src/components/SplashPage/SplashFooter.js
@@ -1,29 +1,29 @@
 const SplashFooter = () => {
     return (
         <div className="splash-footer bounded-1200 centered flex-row">
-            <div className="top-layer flex-row">
-                <div className="tech-icon">
+            <div className="footer-left flex-row">
+                <p>Project repo</p>
+                <div className="tech-icon flex-row">
                     <a href="https://github.com/adamtang5/aa_mod7_capstone_project" target="_blank" rel="noreferrer">
                         <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/github/github-original.svg" alt="project on github" />
                     </a>
                 </div>
+            </div>
+            <div className="footer-middle">
+                <p>Copyright © 2022 Adam Tang</p>
+            </div>
+            <div className="footer-right flex-row">
                 <div className="creator-card">
                     <a href="https://github.com/adamtang5/" target="_blank" rel="noreferrer">
                         <div className="tech-icon">
                             <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/github/github-original.svg" alt="project on github" />
-                        </div>
-                        <div className="creator-avatar">
-                            <img src="https://avatars.githubusercontent.com/u/95706331?v=4" alt="Adam Tang" />
                         </div>
                     </a>
                 </div>
                 <div className="creator-card">
                     <a href="https://linkedin.com/in/adamtangx/" target="_blank" rel="noreferrer">
                         <div className="tech-icon">
-                            <img src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/linkedin/linkedin-original.svg" alt="project on github" />
-                        </div>
-                        <div className="creator-avatar">
-                            <img src="https://avatars.githubusercontent.com/u/95706331?v=4" alt="Adam Tang" />
+                            <img className="no-filter" src="https://raw.githubusercontent.com/devicons/devicon/v2.15.1/icons/linkedin/linkedin-original.svg" alt="project on github" />
                         </div>
                     </a>
                 </div>
